ecdce3ca22560eca3d85096ca30a9852a934afb8
Sohila Hamdy <sohila.fathy22@gmail.com.com>
2024-10-17 11:33:37 +0000

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Defines the minOperations function that calculates the fewest number of
+operations to achieve n H characters using only Copy All and Paste operations.
+"""
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations needed to result in exactly n H characters.
+    If n is impossible to achieve, return 0.
+    
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The fewest number of operations required.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        # While n is divisible by the factor, divide and add operations
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+minimume operations readme
